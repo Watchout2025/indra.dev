3b02dc3882ef1abcd165123a58d7f3c4e87b7226
Watchout2025 <watchoutofficial2006@gmail.com>
2026-02-14 18:49:57 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,20 @@
-const CACHE_NAME = 'IndraAI-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/icons/apple-touch-icon.png',
-  '/icons/favicon.svg'
-];
-
-// Install SW
+// Register the service worker
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+    caches.open('my-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html'
+      ]);
     })
   );
-  self.skipWaiting();
 });
 
-// Fetch and serve
+// Handle fetch requests
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
-  );
-});
-
-// Activate and cleanup old caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames
-          .filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      )
-    )
   );
 });
